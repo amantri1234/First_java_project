cd75b496461736abc97470ecf7120e29b40e96b2
Aman Trivedi <amantrivedi1107@gmail.com>
2025-12-24 22:34:42 +0530

    second

---
+++ myfirstproject.java
@@ -0,0 +1,19 @@
+package myfirstproject;
+
+public class myfirstproject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+class SecondClass {
+    public void displayMessage() {
+        System.out.println("This is the SecondClass in the same file.");
+    }
+}
+
+class ThirdClass {
+    public void showInfo() {
+        System.out.println("This is the ThirdClass in the same file.");
+    }
+}
